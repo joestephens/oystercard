580f89f0db84aa32d653d1367d5e31f8132ab877
Hassan Radheyyan <hassanradheyyan@Hassans-MacBook-Pro.local>
2016-05-30 13:14:48 +0100

    Set #top_up LIMIT

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
   DEFAULT_BALANCE=0
+  LIMIT=90
 
   attr_reader :balance
 
@@ -9,7 +10,14 @@ class Oystercard
   end
 
   def top_up(money)
+    fail "Card is at its limit of #{LIMIT}." if at_limit?(money)
     @balance += money
+  end
+
+  private
+
+  def at_limit?(money)
+    balance+money > LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,13 +2,22 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
+  describe '#balance' do
+    it 'has a balance of zero' do
+      expect(subject.balance).to eq(0)
+    end
   end
+  
+  describe '#top_up' do
+    it 'allows for topping up 10 onto card' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
 
-  it 'allows for topping up 10 onto card' do
-    subject.top_up(10)
-    expect(subject.balance).to eq 10
+    it "doesn't allow topping up above its limit" do
+      max_limit = described_class::LIMIT
+      subject.top_up(max_limit)
+      expect{subject.top_up(1)}.to raise_error(RuntimeError,"Card is at its limit of #{max_limit}.")
+    end
   end
-
 end
